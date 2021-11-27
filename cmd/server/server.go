150dbea60f62fbb0c7921067ece67c2ce54e4e57
zhyoulun <zhyoulun@gmail.com>
2021-11-27 18:18:02 +0800

    add grpc gateway server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	pb "github.com/zhyoulun/grpc-example/proto/sdk/proto"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
+	"sync"
 )
 
 type server struct {
@@ -20,12 +23,43 @@ func (s *server) Ping(ctx context.Context, in *pb.PingRequest) (*pb.PingResponse
 }
 
 func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go runGRPCServer(wg)
+	go runGateway(wg)
+	wg.Wait()
+}
+
+func runGateway(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterHelloHandlerFromEndpoint(ctx, mux, "127.0.0.1:12345", opts)
+	if err != nil {
+		log.Fatalf("pb RegisterHelloHandlerFromEndpoint fail: %+v", err)
+	}
+
+	log.Printf("12345 for grpc, 12346 for grpc gateway")
+
+	err = http.ListenAndServe("127.0.0.1:12346", mux)
+	if err != nil {
+		log.Fatalf("http ListenAndServe fail: %+v", err)
+	}
+}
+
+func runGRPCServer(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &server{})
+
 	ln, err := net.Listen("tcp", "127.0.0.1:12345")
 	if err != nil {
 		log.Fatalf("net Listen fail: %+v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterHelloServer(s, &server{})
 	log.Printf("server listening at %+v", ln.Addr())
 	if err := s.Serve(ln); err != nil {
 		log.Fatalf("failed to serve: %+v", err)
