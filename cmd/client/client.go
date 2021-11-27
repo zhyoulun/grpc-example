917de015d6f95888448f3776a0880fd89176ec7b
zhyoulun <zhyoulun@gmail.com>
2021-11-27 17:14:57 +0800

    init grpc example

---
+++ cmd/client/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	pb "github.com/zhyoulun/grpc-example/proto"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:12345", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("grpc Dial fail: %+v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewHelloClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Ping(ctx, &pb.PingRequest{
+		Name: "haha",
+	})
+	if err != nil {
+		log.Fatalf("c Ping fail: %+v", err)
+	}
+	log.Printf("ping response: %+v", r.GetContent())
+}
---
+++ cmd/server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	pb "github.com/zhyoulun/grpc-example/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedHelloServer
+}
+
+func (s *server) Ping(ctx context.Context, in *pb.PingRequest) (*pb.PingResponse, error) {
+	log.Printf("receive request: %+v", in.GetName())
+	return &pb.PingResponse{
+		Content: "pong " + in.GetName(),
+	}, nil
+}
+
+func main() {
+	ln, err := net.Listen("tcp", "127.0.0.1:12345")
+	if err != nil {
+		log.Fatalf("net Listen fail: %+v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &server{})
+	log.Printf("server listening at %+v", ln.Addr())
+	if err := s.Serve(ln); err != nil {
+		log.Fatalf("failed to serve: %+v", err)
+	}
+}
